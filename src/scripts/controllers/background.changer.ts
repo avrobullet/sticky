f70e6d8f15a60790b8274230cae03b89ee037609
avrobullet <alexanderdouglas43@gmail.com>
2024-04-21 22:46:19 -0700

    Finished up with background work

--- src/scripts/controllers/background.changer.ts
+++ src/scripts/controllers/background.changer.ts
@@ -1,8 +1,18 @@
 
 // Initialize variables
-var colors = ['#ff0000', '#000000']; // [day, night]
-var night_colours = ['#36454F', '#006400', '#808080', '#800080', '#191970']
-var time_change = 5000
+var day_colours = [
+    '#87CEEB', // Sky Blue 
+    '#90EE90', // Light Green 
+    '#F5F5DC', // Beige
+    '#D3D3D3', // Light Gray 
+    '#FFFF99'];// Pastel Yellow
+var night_colours = [
+    '#36454F', // Charcoal
+    '#013220', // Dark Green
+    '#808080', // Dark Gray 
+    '#800080', // Dark Purple 
+    '#191970'];// Midnight Blue
+var time_change = 5000;
 
 // Just a class to alternate between background colors (will expand to be more )
 export default class BackgroundController {
@@ -15,12 +25,12 @@ export default class BackgroundController {
 
     // Check if it's night time (e.g., between 7pm and 6am)
     changeBackgroundDayNight() {
-        // Get time
+        // Get current time
         this.hour = new Date().getHours();
 
         //Change background color
-        if (this.hour >= 19 || this.hour < 6) {
-            document.body.style.backgroundColor = night_colours[3];
+        if (7 < this.hour && this.hour < 17) {
+            document.body.style.backgroundColor = day_colours[Math.floor(Math.random() * day_colours.length)];
         } else {
             document.body.style.backgroundColor = night_colours[Math.floor(Math.random() * night_colours.length)];
         }

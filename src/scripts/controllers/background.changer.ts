deef35e3d0ad80c81b087362757ccdfa481e13a9
avrobullet <alexanderdouglas43@gmail.com>
2024-04-21 18:40:31 -0700

    Added list of night colours to be randomly selected

--- src/scripts/controllers/background.changer.ts
+++ src/scripts/controllers/background.changer.ts
@@ -1,13 +1,15 @@
 
 // Initialize variables
 var colors = ['#ff0000', '#000000']; // [day, night]
-var time_change = 1000
+var night_colours = ['#36454F', '#006400', '#808080', '#800080', '#191970']
+var time_change = 5000
 
 // Just a class to alternate between background colors (will expand to be more )
 export default class BackgroundController {
     private hour: number;
 
     constructor() {
+        this.changeBackgroundDayNight();
         this.alternateBackgroundColor();
     }
 
@@ -18,9 +20,9 @@ export default class BackgroundController {
 
         //Change background color
         if (this.hour >= 19 || this.hour < 6) {
-            document.body.style.backgroundColor = colors[0];
+            document.body.style.backgroundColor = night_colours[3];
         } else {
-            document.body.style.backgroundColor = colors[1];
+            document.body.style.backgroundColor = night_colours[Math.floor(Math.random() * night_colours.length)];
         }
     }
 
